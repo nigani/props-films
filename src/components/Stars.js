4963b708144318488de82ad6ebb6b2a587b166a7
nigani <nigani@internet.ru>
2022-06-16 21:26:04 +0300

    2 files modified

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -5,14 +5,18 @@ import { nanoid } from 'nanoid';
 
 export default function Stars(props) {
   const { count } = props;
-  return (
-    count >= 1 & count <= 5
-    ? <ul class="card-body-stars u-clearfix">
-      {Array(count).fill().map(_ => (
-        <Star key={nanoid()}/>
-      ))}
-    </ul> 
-    : <></>
+  return (count >= 1) & (count <= 5) ? (
+    <ul class="card-body-stars u-clearfix">
+      {Array(count)
+        .fill()
+        .map((_) => (
+          <li>
+            <Star key={nanoid()} />
+          </li>
+        ))}
+    </ul>
+  ) : (
+    <></>
   );
 }
 
